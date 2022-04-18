08c1230f33d9cd8d3b6bea671d1b4cb3cae5f59e
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-18 16:18:39 +0000

    Database sql commands added

--- Controller/server.js
+++ Controller/server.js
@@ -1,4 +1,8 @@
 import express from "express";
+import cors from "cors";
+
+app.use(cors());
+app.use(express.json());
 
 const app = express();
 const PORT = 5000;
---
+++ Model/octosenda.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE octosenda;
+
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY,
+    user_first_name VARCHAR(255) NOT NULL,
+    user_last_name VARCHAR(255) NOT NULL,
+    username VARCHAR(255) UNIQUE,
+    user_email VARCHAR(255) UNIQUE NOT NULL,
+    user_password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE receiver(
+    receiver_id SERIAL PRIMARY KEY,
+    receiver_first_name VARCHAR(255),
+    receiver_last_name VARCHAR(255),
+    receiver_contact VARCHAR(255) NOT NULL 
+);
+
+CREATE TABLE message(
+    message_id SERIAL PRIMARY KEY,
+    message_subject VARCHAR(255),
+    message_body VARCHAR(255),
+    date_time_sent TIMESTAMP,
+    message_receiver VARCHAR(255),
+    saved_as_template BOOLEAN,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE message_receiver(
+    receiver_id INT,
+    message_id INT,
+    PRIMARY KEY (receiver_id, message_id),
+    FOREIGN KEY (receiver_id) REFERENCES receiver(receiver_id),
+    FOREIGN KEY (message_id) REFERENCES message(message_id)
+);
