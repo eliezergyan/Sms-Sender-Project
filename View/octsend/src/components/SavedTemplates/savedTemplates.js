c6eb00248be58f8725f1e3ec4975592f761da1ad
George Hammond <george.hammond@amalitech.org>
2022-04-21 02:27:34 +0000

    Commit minor changes

--- View/octsend/src/components/SavedTemplates/savedTemplates.js
+++ View/octsend/src/components/SavedTemplates/savedTemplates.js
@@ -43,21 +43,7 @@ const SavedTemplates = () =>{
                     <button>Edit</button>
                     <button>Send Now</button>
                 </div>                
-            </div>
-
-            <div className="hisory-message-section">
-                <div className="hisory-message">
-                    <div>
-                        <h3>1506</h3>
-                        <p id="subject">Is your balance low?</p>
-                        <p id="message">No worries you are eligible for SOS</p>
-                    </div>                    
-                </div>
-                <div className="btn">
-                    <button>Edit</button>
-                    <button>Send Now</button>
-                </div>                
-            </div>
+            </div>            
         </div>
     )
 }
