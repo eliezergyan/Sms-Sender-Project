09f5200cd2031727155069505fc8b5aee9413921
George Hammond <george.hammond@amalitech.org>
2022-04-21 06:30:40 +0000

    Main page alterations

--- View/octsend/src/components/history/history.js
+++ View/octsend/src/components/history/history.js
@@ -59,6 +59,34 @@ const History = () =>{
                 </div>                
             </div>
 
+            <div className="hisory-message-section">
+                <div className="hisory-message">
+                    <div>
+                        <h3>1506</h3>
+                        <p id="subject">Is your balance low?</p>
+                        <p id="message">No worries you are eligible for SOS</p>
+                    </div>                    
+                </div>
+                <div className="btn">
+                    <button>Edit</button>
+                    <button>Send Now</button>
+                </div>                
+            </div>
+
+            <div className="hisory-message-section">
+                <div className="hisory-message">
+                    <div>
+                        <h3>1506</h3>
+                        <p id="subject">Is your balance low?</p>
+                        <p id="message">No worries you are eligible for SOS</p>
+                    </div>                    
+                </div>
+                <div className="btn">
+                    <button>Edit</button>
+                    <button>Send Now</button>
+                </div>                
+            </div>
+
             
         </div>
     )
