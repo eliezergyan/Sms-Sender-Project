daf3059915413d0ef91915796b7e5bed6fc975a9
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 23:41:24 +0000

    Changed * to /

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'View/octsend/build')));
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./View/octsend/build/index.html"));
 });
 
