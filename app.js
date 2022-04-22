0de2a33b1f1ae231c830a534025a07358549fd90
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 23:29:12 +0000

    added static

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('View/octsend/build'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
