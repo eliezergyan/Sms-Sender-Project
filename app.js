75c418b7068a2a6f9422efe405d2a909feb6eacc
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 23:52:24 +0000

    Changed back to true

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static(path.join(__dirname, 'View/octsend/build')));
 
