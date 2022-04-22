af00969eaf5b4316525ee6b906a7e4f3409ef7b9
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 22:57:04 +0000

    Changed path to *

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", function (req, res) {
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./View/octsend/build/index.html"));
 });
 
