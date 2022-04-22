110489384057e73bf8c54faec73eb48cafe1dbc3
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 14:10:10 +0000

    Changed the PORT number

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const templateRoute = require("./Controller/routes/templateRoute");
 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
