1bdffe81dae5f6f3e6e96ac7a0298fe4b88345e2
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 23:33:49 +0000

    added path to it

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('View/octsend/build'));
+app.use(express.static(path.join(__dirname, 'View/octsend/build')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
