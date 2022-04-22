a182cce103948bdee2f70900fa480e3f27fe13dd
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 14:28:36 +0000

    testing the entry point

--- app.js
+++ app.js
@@ -10,9 +10,7 @@ const templateRoute = require("./Controller/routes/templateRoute");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send("Hello there");
-})
+app.get('/', loginRouter)
 
 app.use(cors());
 app.use(express.json());
