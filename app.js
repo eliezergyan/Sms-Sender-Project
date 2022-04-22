51b445d836de25fdf3cb3519aa2ff4daa285a662
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 23:11:57 +0000

    Changed to /

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ if (process.env.NODE_ENV === "production") {
     app.use(express.static("View/octsend/build"));
   }
 
-app.get("*", function (req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./View/octsend/build/index.html"));
 });
 
