c7857b40fd2611bf878d44cf1ab35b5f207d9429
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-04-22 23:38:06 +0000

    changed extended to false

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "./View/octsend/build/index.html"));
