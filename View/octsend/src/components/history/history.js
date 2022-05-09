c891504cd152bd0b22dd8e552a723778ea6983ce
eliezergyan <91868126+eliezergyan@users.noreply.github.com>
2022-05-09 09:08:11 +0000

    Added resend feature

--- View/octsend/src/components/history/history.js
+++ View/octsend/src/components/history/history.js
@@ -6,6 +6,45 @@ const History = () =>{
 
     const [history, setHistory] = useState([]);
 
+    const Resend = ({message}) => {
+        const [messageSubject, setMessageSubject] = useState(message.message_subject);
+        const [messageBody, setMessageBody] = useState(message.message_body);
+        const [contacts, setContacts] = useState(message.receiver_contact);
+    
+    
+        const handleSend = async (e) => {
+            e.preventDefault();
+            try {
+                const splitContacts = contacts.split(";");
+                const body = {messageBody, splitContacts};
+                const response = await fetch("http://localhost:5000/messages/send_message", {
+                    method: "POST",
+                    headers: {"content-type": "application/json"},
+                    body: JSON.stringify(body)
+                });
+                
+                splitContacts.forEach(async (messageReceiver) => {
+                    const body =  { messageSubject, messageBody, messageReceiver };
+                    const response = await fetch("http://localhost:5000/messages", {
+                        method: "POST",
+                        headers: {"content-type": "application/json"},
+                        body: JSON.stringify(body)
+                    });
+                })
+    
+    
+            } catch (err) {
+                console.error(err.message)
+            }
+        }
+    
+    
+    
+        return(
+            <button data-target={`#id${message.message_id}`} onClick={handleSend}>Resend</button>
+        )
+    }
+
 
     const getHistory = async () => {
         try {
